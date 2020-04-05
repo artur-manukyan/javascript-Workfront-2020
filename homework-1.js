d5ee8ec03aa4b2fcc6ea0fc0de27ff289ae91e26
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-05 17:26:29 +0400

    Update homework-1.js

--- homework-1.js
+++ homework-1.js
@@ -7,3 +7,38 @@ givenNum % 2 === 0 ? console.log('Even') : console.log('Odd');
 
 // 2. Given two numbers print 1 if one of them is divisible by the other one, otherwise print 0.
 
+let input1 = prompt('Input 1');
+let input2 = prompt('Input 2');
+let givenNum1 = +input1;
+let givenNum2 = +input2;
+givenNum1 % givenNum2 === 0 || givenNum2 % givenNum1 === 0 ? console.log(1) : console.log(0);
+
+
+/* 3. Given two variables, which are the angles of a triangle. Find the third angle of the
+triangle. (Sum of the angles of a triangle equals 180 degrees).*/
+
+let input1 = prompt('Input 1');
+let input2 = prompt('Input 2');
+let givenNum1 = +input1;
+let givenNum2 = +input2;
+let result = 180 - (givenNum1 + givenNum2);
+console.log(result);
+
+
+// 4. Given number n (positive integer). Print the value of n + nn + nnn(not multiplication).
+
+let input = prompt('Input');
+let givenNum = +input;
+let secondPart = String(input * 2);
+let thirdPart = String(input * 3);
+let givenNumString = String(givenNum);
+let resultString = givenNumString + secondPart + thirdPart;
+let resultNum = +resultString;
+console.log(resultNum);
+
+
+/*5. Given a positive integer. Bring the last digit of the number to the beginning. Print the new
+number. If the last digit of the inserted number is 0, number remains the same.*/
+
+
+
