39a714a2dcebd15e6e588b05288e5916447156bf
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-05 18:47:08 +0400

    Create exercise-6.js

---
+++ homework-1/exercise-6.js
@@ -0,0 +1,15 @@
+/* 6. Given five numbers as input. Calculate and print the average of the numbers(without
+using arrays).*/
+
+let input1 = prompt('Input 1');
+let input2 = prompt('Input 2');
+let input3 = prompt('Input 3');
+let input4 = prompt('Input 4');
+let input5 = prompt('Input 5');
+let givenNum1 = +input1;
+let givenNum2 = +input2;
+let givenNum3 = +input3;
+let givenNum4 = +input4;
+let givenNum5 = +input5;
+let result = (givenNum1 + givenNum2 + givenNum3 + givenNum4 + givenNum5) / 5;
+console.log(result);
