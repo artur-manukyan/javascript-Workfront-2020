a7bc7bad9ffd82a6c66ab3479ba43d9d692477a3
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-06 22:31:38 +0400

    Create exercises.js

---
+++ homework-1/exercises.js
@@ -0,0 +1,4 @@
+/*8. Given an age, figure out whether someone is a baby(1 months - 12 months), toddler (1
+year - 2 years), child(3 years - 12 years ), teenager(13 years - 17 years) or adult(18
+years and more ). Also check that age in months is between 1 and 12.*/
+
