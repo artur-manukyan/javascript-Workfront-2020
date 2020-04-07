7bcd15b35edb015cfd77584205fcd957a8941570
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-08 02:05:11 +0400

    Delete exercise-3.js

--- homework-1/exercise-3.js
+++
@@ -1,9 +0,0 @@
-/* 3. Given two variables, which are the angles of a triangle. Find the third angle of the
-triangle. (Sum of the angles of a triangle equals 180 degrees).*/
-
-let input1 = prompt('Input 1');
-let input2 = prompt('Input 2');
-let givenNum1 = +input1;
-let givenNum2 = +input2;
-let result = 180 - (givenNum1 + givenNum2);
-console.log(result);
