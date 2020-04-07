8bbd8b798a10ab99dfa053cee7e2a6c65a2be141
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-08 02:04:57 +0400

    Delete exercise-2.js

--- homework-1/exercise-2.js
+++
@@ -1,7 +0,0 @@
-// 2. Given two numbers print 1 if one of them is divisible by the other one, otherwise print 0.
-
-let input1 = prompt('Input 1');
-let input2 = prompt('Input 2');
-let givenNum1 = +input1;
-let givenNum2 = +input2;
-givenNum1 % givenNum2 === 0 || givenNum2 % givenNum1 === 0 ? console.log(1) : console.log(0);
