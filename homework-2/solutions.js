5700d791653293481cce9a598ae19e980351b899
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-12 00:00:02 +0400

    Update solutions.js

--- homework-2/solutions.js
+++ homework-2/solutions.js
@@ -1,9 +1,36 @@
 // 1. Insert a number. Print ‘yes’ if the number is prime, ‘no’ otherwise.
-
 function isPrime(num){ 
     counter = 0;
     for(i = num; i > 0; i--){
         num % i === 0 ? counter++ : counter;
     }
     return counter > 2 ? 'No' : 'Yes'
+}
+
+
+// 2. Given a number n ( n>= 0 ). Print nth Fibonacci number.  (Fibonacci series: 0, 1, 1, 2, 3, 5, 8 …, ak = ak-1 + ak-2)
+function fib_n(position){
+    fibArray = [0, 1]
+    for (let i = 1; true; i++){
+        if (position === 0){return 0;}
+        else{
+            fibArray.push(fibArray[i] + fibArray[i-1])
+            if(i === position){return fibArray[i]}
+        }
+    }
+}
+
+
+// 3. Given a number n( n> 0 ). Print Fibonacci series up to n.
+function fib_untill_n(threshold){
+    fibArray = [0, 1]
+    for (let i = 1; true; i++){
+        if (threshold === 0 || threshold === 1){return 0;}
+        else{
+            fibArray.push(fibArray[i] + fibArray[i-1])
+            if(fibArray[i+1] < threshold){continue;}
+            fibArray.pop();
+            return fibArray;
+        }
+    }
 }
