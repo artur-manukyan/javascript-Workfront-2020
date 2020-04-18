8afeba823aeb71be2ab098d3598030ef29574934
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-18 23:47:50 +0400

    Update solutions.js

--- homework-3/solutions.js
+++ homework-3/solutions.js
@@ -10,3 +10,21 @@ function solution1(num){
 
 /* 2.  Given an array of numbers. Write a recursive function to find its minimal positive element. 
 (if such element does not exist, return -1).*/
+function solution2(arr) {
+var counter = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] < 0){
+            arr[i] = +Infinity;
+            counter++
+        }
+        if(counter === arr.length){
+            return -1;
+        }
+    }
+    if (arr.length < 2) {
+        return arr[0];
+    } else {
+        let min = Math.min(arr.shift(), solution2(arr));
+        return min;
+    } 
+}
