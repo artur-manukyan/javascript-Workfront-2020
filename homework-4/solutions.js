8ec65e60abb5aa08e1630ee19f8dac32569f2d94
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-04-26 20:13:11 +0400

    Update solutions.js

--- homework-4/solutions.js
+++ homework-4/solutions.js
@@ -13,3 +13,59 @@ function solution1(arr=[], i = arr.length - 1){
 
 // 2. Given an object. Invert it (keys become values and values become keys). 
 // If there is more than key for that given value create an array.
+function solution2(obj) {
+    let entries = Object.entries(obj);
+    entries.forEach(entry => {
+    [entry[0], entry[1]] = [entry[1], entry[0]];
+    });
+    
+    for (let i = 0; i < entries.length; i++) {
+        for (let j = i+1; j < entries.length; j++) {
+            if (entries[i][0] === entries[j][0]) {
+                entries[i].push(entries[j][1]);
+                entries[j].push('remove this');
+            }
+        }
+    }
+
+    let uniques = []
+    for (let i = 0; i < entries.length; i++) {
+        if (entries[i].indexOf('remove this') === -1) {
+            uniques.push(entries[i]);
+        }
+    }
+
+    let resObj = {}
+    for (let i = 0; i < uniques.length; i++) {
+        if (uniques[i].length > 2) {
+            resObj[uniques[i][0]] = uniques[i].slice([1]);
+        } else {
+            resObj[uniques[i][0]] = uniques[i][1];
+        }
+    }
+return resObj;
+}
+
+
+/*3. Given the list of the following readers:
+ [
+ { book: "Catcher in the Rye", readStatus: true, percent: 40},
+ { book: "Animal Farm", readStatus: true, percent: 20},
+ { book: "Solaris", readStatus: false, percent: 90 },
+ { book: "The Fall", readStatus: true, percent: 50 },
+ { book: "White Nights", readStatus: false, percent: 60 } ,
+ { book: "After Dark", readStatus: true, percent: 70 }
+ ];
+ Output the books sorted by the percent in descending order which readStatus is true.
+*/
+let sortedBooks = books.sort(function (a, b) {
+    if (a.readStatus < b.readStatus) {return 1}
+    if (a.readStatus > b.readStatus) {return -1}    
+
+    if (a.percent < b.percent) {return 1}
+    if (a.percent > b.percent) {return -1}
+});
+
+
+// 4. Given an array and a number N.  Write a recursive function that rotates an array N places to the left. 
+// (Hint: to add element to the beginning use arr.unshift())
