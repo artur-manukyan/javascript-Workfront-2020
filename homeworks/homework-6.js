86a2bd8a535bca54d304af903ef9c5818db85465
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-05-20 23:48:46 +0400

    Update homework-6.js

--- homeworks/homework-6.js
+++ homeworks/homework-6.js
@@ -1,5 +1,5 @@
 // 1. Given an array. Determine whether it consists only from unique elements or not.
-function solution1(givenArray) {
+function solution1(givenArray){
     let setFromArray = new Set(givenArray);
     return givenArray.length === setFromArray.size?
             'unique elements':
