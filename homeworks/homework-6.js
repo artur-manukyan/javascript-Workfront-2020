3f0fed3cc80144fbe48ec263fe86ec395b75465b
artur-manukyan <63193150+artur-manukyan@users.noreply.github.com>
2020-05-21 00:34:29 +0400

    Update homework-6.js

--- homeworks/homework-6.js
+++ homeworks/homework-6.js
@@ -5,3 +5,12 @@ function solution1(givenArray){
             'unique elements':
             'there are duplicates';
 }
+
+
+// 2. Given an array of numbers. Find the sum of numbers’ quadratic which are even.
+function solution2(givenArray){
+    let quadraticEvensSum = givenArray.reduce((accumulator, currentValue) => {
+        return !(((currentValue)**2) % 2) ? accumulator + currentValue : accumulator    
+    }, 0)
+    return quadraticEvensSum;
+}
